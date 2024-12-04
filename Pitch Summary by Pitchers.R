95cef3d0ce74206091715fee16eccc649e8e1118
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-12-04 15:54:31 -0500

    Update Pitch Summary by Pitchers.R

--- Pitch Summary by Pitchers.R
+++ Pitch Summary by Pitchers.R
@@ -9,18 +9,20 @@ slider = data[data$AutoPitchType=="Slider", ]
 
 describeBy(slider[c("HorzBreak","VertBreak", "RelSpeed")], group = slider$Pitcher)
 
-
-
 #---------------------------Curveball---------------------
 
 curve = data[data$AutoPitchType=="Curveball", ]
-
 
 # -------(Horizontal Break, Vertical Break, and Release Speed) of slider Summary by Pitcher-------
 
 describeBy(curve[c("HorzBreak","VertBreak", "RelSpeed")], group = curve$Pitcher)
 
 
-
-#------------#the mean velocity of ALL pitches for EACH Pitcher
+#---------------------#the mean velocity of ALL pitches for EACH Pitcher-------------------
 aggregate(data$RelSpeed~data$Pitcher, data = data, mean) 
+
+                #------OUTPUT----------
+                #   baseball$Pitcher baseball$RelSpeed
+                #  1     Ortega, Nick          78.17913
+                #  2      Parks, Evan          76.86553
+                #  3    Sipling, Eric          79.68019
