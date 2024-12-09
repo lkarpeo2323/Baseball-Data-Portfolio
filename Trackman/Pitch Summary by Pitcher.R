07335206b4478beb5c84bcd6eae4701eb465aed8
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-12-08 23:10:56 -0500

    Update and rename Pitch Analysis.py to Pitch Summary by Pitcher.R

---
+++ Trackman/Pitch Summary by Pitcher.R
@@ -0,0 +1,18 @@
+library(psych)
+data = read.csv("data.csv")
+
+#---------------------------Slider---------------------
+
+slider = data[data$AutoPitchType=="Slider", ]
+
+# -------(Horizontal Break, Vertical Break, and Release Speed) of slider Summary by Pitcher-------
+
+describeBy(slider[c("HorzBreak","VertBreak", "RelSpeed")], group = slider$Pitcher)
+
+#---------------------------Curveball---------------------
+
+curve = data[data$AutoPitchType=="Curveball", ]
+
+# -------(Horizontal Break, Vertical Break, and Release Speed) of slider Summary by Pitcher-------
+
+describeBy(curve[c("HorzBreak","VertBreak", "RelSpeed")], group = curve$Pitcher)
